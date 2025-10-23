808aa142f446df317c768acfa65579c8e6c3c267
merwico <51908159+merwico@users.noreply.github.com>
2025-10-22 20:37:01 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -33,10 +33,34 @@ def main():
 
     # CREO LA CARPETA PARA GUARDAR LA INFO
     os.makedirs(carpeta, exist_ok=True)
+    
+    # Valido si es que existen columnas en mi archivo
+    columna = df.select_dtypes(include=[np.number])
+    if columna.empty:
+        print("\n.....No hay columnas para graficar.....")
+        return
+        
     # PONIENDO NOMBRE AL ARCHIVO
     fecha = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     ruta = os.path.join(carpeta, f"Grafico_ventas_{fecha}.png")
-    
+
+    # GENERO LOS GRAFICOS
+    try:
+        plt.figure(figsize=(10, 6))
+        columna.plot(ax=plt.gca())
+        plt.title("📈 Análisis de Ventas - Columnas Numéricas")
+        plt.xlabel("Registros")
+        plt.ylabel("Valores")
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig(ruta)
+        plt.close()
+        print(f"\n.....Gráfico generado y guardado en: {ruta}")
+    except Exception as e:
+        print(f".....Error al generar el gráfico: {e}")
+        return
+
+#Llamo la funcion
 if __name__ == "__main__":
-main()
+    main()
 
