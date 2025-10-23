df4259bdbcee6f49d349c5abdcc439537ff394c1
merwico <51908159+merwico@users.noreply.github.com>
2025-10-22 19:49:49 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
+# DEFININDO LOS ARCHIVOS
+archivo = "data.csv"
+
+def main():
+    print("🚀 Iniciando análisis automático de datos de ventas...")
+    print(f"📂 Archivo de origen: {archivo}")
+
+    # VERIFICAMOS QUE EXISTA EL ARCHIVO
+    if not os.path.exists(archivo):
+        print(f"❌ Error: No se encontró el archivo '{archivo}'.")
+        return
+
+    # LEER EL ARCHIVO CSV CON LA INFORMACINO
+    try:
+        df = pd.read_csv(archivo)
+    except Exception as e:
+        print(f"❌ Error al leer el archivo CSV: {e}")
+        return
