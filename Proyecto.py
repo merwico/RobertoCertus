72f6297f25250b5577121bb9c27a506282d48e16
merwico <51908159+merwico@users.noreply.github.com>
2025-10-22 20:50:36 -0500

    Validacion del archivo

--- Proyecto.py
+++ Proyecto.py
@@ -11,3 +11,18 @@ carpeta = "Resultados"
 def main():
     print("🚀 Iniciando análisis automático de datos de ventas")
     print(f"📂 Archivo de origen: {archivo}")
+
+     # Verifico que el archivo exista
+    if not os.path.exists(archivo):
+        print(f"❌ Error: No se encontró el archivo '{archivo}'.")
+        return
+    # Lectura del archivo
+    try:
+        df = pd.read_csv(archivo)
+    except Exception as e:
+        print(f"❌ Error al leer el archivo CSV: {e}")
+        return
+    # Valido si esta vacio el archivo
+    if df.empty:
+        print("⚠️ El archivo CSV está vacío. No se generará gráfico.")
+        return
